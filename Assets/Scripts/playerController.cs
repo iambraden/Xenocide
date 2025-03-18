152976772e803e44f8893b518b32eea1b082d969
iambraden <iambradencollins@outlook.com>
2025-03-18 14:17:16 -0700

    added dynamic sprites depending on user input

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
+
+    [Header("Player movement")]
     public float moveSpeed = 5f;
     public float xMin = -4.6f; // Left boundary
     public float xMax = 4.6f;  // Right boundary
+
+    [Header("Player sprites")]
+    public SpriteRenderer playerSpriteRenderer;
+    public Sprite idleSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
 
     void FixedUpdate()
     {
@@ -19,5 +27,21 @@ public class playerController : MonoBehaviour
         Vector2 clampedPosition = playerRigidbody.position;
         clampedPosition.x = Mathf.Clamp(clampedPosition.x, xMin, xMax);
         playerRigidbody.position = clampedPosition;
+        
+        // Change player sprite based on movement
+        if (horizontalInput < 0){ 
+
+        playerSpriteRenderer.sprite = leftSprite;
+
+        }else if (horizontalInput > 0){
+
+        playerSpriteRenderer.sprite = rightSprite;
+
+        }else{
+
+        playerSpriteRenderer.sprite = idleSprite;
+
+        }
+        
     }
 }
