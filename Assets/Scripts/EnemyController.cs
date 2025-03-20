ab1e5efde785b9b67e6df5db43b3dd07dc6925a4
iambraden <113061590+iambraden@users.noreply.github.com>
2025-03-20 14:53:34 -0700

    Added sway sprites + animation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Movement
+    public enum EnemyType { Wave, Sway };
+    public EnemyType enemyType;
+
+    public Sprite[] animationSprites;
+    public float animationTime = 1.0f;
+
+    private SpriteRenderer spriteRenderer;
+    private int animationFrame;
+
+    [Header("Movement")]
     public float speed = 2f;
     private Vector2 startPos;
     public float maxDistance = 5f;
     public float frequency = 2f;
     public float phaseOffset;
 
-    // Enemy Type
-    public enum EnemyType { Wave, Sway }
-    public EnemyType enemyType;
-
-    // Health
+    [Header("Health")]
     public int health = 3;
 
-    // Shooting
+    [Header("Shooting")]
     public GameObject bulletPrefab;
     public Transform firePoint;
     private float fireTimerLow = 2f;
@@ -24,12 +29,18 @@ public class EnemyController : MonoBehaviour
     private float firetimer;
     private float minFireHeight = -3.0f;
 
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Start()
     {
         firetimer = Random.Range(fireTimerLow, fireTimerHigh);
         startPos = transform.position;
         phaseOffset = Random.Range(0f, 2f * Mathf.PI); // Random phase offset for variation
+        //animate the enemy
+        InvokeRepeating(nameof(animateSprite), this.animationTime, this.animationTime);
     }
 
     void Update()
@@ -40,15 +51,18 @@ public class EnemyController : MonoBehaviour
         else if (enemyType == EnemyType.Sway)
             SwayMove();
 
-        //fire bullets on a varied timer if the enemy is high enough in the screen
-        firetimer -= Time.deltaTime;
-        if(firetimer <= 0 && transform.position.y > minFireHeight)
+        // Only fire bullets if the enemy type is Wave
+        if (enemyType == EnemyType.Wave)
         {
-            firetimer = Random.Range(fireTimerLow, fireTimerHigh);
-            FireBullet();
+             // fire bullets on a varied timer if the enemy is high enough in the screen
+            firetimer -= Time.deltaTime;
+            if (firetimer <= 0 && transform.position.y > minFireHeight)
+            {
+                firetimer = Random.Range(fireTimerLow, fireTimerHigh);
+                FireBullet();
+            }
         }
     }
-
 
     void WaveMove()
     {
@@ -66,20 +80,17 @@ public class EnemyController : MonoBehaviour
         transform.position = pos;
     }
 
-
     void SwayMove()
     {
         /*TODO: Implement Movement for the enemies that sit 
         near the top of the screen and move in the grid pattern*/
     }
 
-
     void FireBullet()
     {
         GameObject newBulletPrefab = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
         newBulletPrefab.transform.SetParent(GameObject.Find("EnemyBullets").transform);
     }
-
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -94,5 +105,18 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    void animateSprite()
+    {
+        //todo fix index out of bounds
+        if(animationFrame == 0)
+        {
+            animationFrame = 1;
+        }
+        else
+        {
+            animationFrame = 0;
+        }
 
+        spriteRenderer.sprite = this.animationSprites[animationFrame];
+    }
 }
